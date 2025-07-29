95a8a00ae6a736ac42f194f1ffa7cc02c617552c
Karim Azmi <31988473+kariimazmi@users.noreply.github.com>
2025-07-29 12:53:08 +0300

    added initial project and network framework

---
+++ Countries/Countries/CountriesApp.swift
@@ -0,0 +1,17 @@
+//
+//  CountriesApp.swift
+//  Countries
+//
+//  Created by Karim Azmi on 29/7/25.
+//
+
+import SwiftUI
+
+@main
+struct CountriesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
