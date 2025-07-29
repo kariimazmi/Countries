a67c9b8dfcf67ce9de44e38ac8e93bb532710421
Karim Azmi <31988473+kariimazmi@users.noreply.github.com>
2025-07-29 12:57:20 +0300

    updated configurations

--- NetworkKit/NetworkKit/NetworkKit.h
+++ NetworkKit/NetworkKit/NetworkKit.h
@@ -14,5 +14,3 @@ FOUNDATION_EXPORT double NetworkKitVersionNumber;
 FOUNDATION_EXPORT const unsigned char NetworkKitVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <NetworkKit/PublicHeader.h>
-
-
